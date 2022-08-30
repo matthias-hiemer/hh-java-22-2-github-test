9bb52276c9483113a6f6292836376a98293b822e
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-08-30 17:14:54 +0200

    Add Rest of the World feature

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hallo GitHub!");
+        System.out.println("Und Hallo Rest der Welt!");
     }
 }
