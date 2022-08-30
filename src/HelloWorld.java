6ab63af3e39e2de02a7742a7dc2a996fd204f69b
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-08-30 17:22:19 +0200

    Add fancy people greeting

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hallo GitHub!");
         System.out.println("Und Hallo Rest der Welt!");
+
+        System.out.println("Und Hallo fancy People!");
     }
 }
